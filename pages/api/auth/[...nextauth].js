eb7967a774b3274af907e4a0864aeb35ebc02288
karankashyap04 <karankashyap@gmail.com>
2022-10-29 16:01:49 -0400

    Apply prettier styles

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,9 +5,8 @@ export default NextAuth({
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
     ],
-    secret: process.env.JWT_SECRET
+    secret: process.env.JWT_SECRET,
 });
-
