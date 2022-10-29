0526cf87455d13e58f066d528beb2940168590e0
karankashyap04 <karankashyap@gmail.com>
2022-10-29 15:48:49 -0400

    Fix styles to match prettier documentation

--- models/User.ts
+++ models/User.ts
@@ -1,46 +1,46 @@
-import { model, Schema } from 'mongoose';
+import { model, Schema } from "mongoose";
 
 enum userRole {
     Admin = 0,
     Writer = 1,
-    Reader = 2
-};
+    Reader = 2,
+}
 
 const userSchema = new Schema({
     userID: {
         type: Number,
-        required: true
+        required: true,
     },
     name: {
         type: String,
-        required: true
+        required: true,
     },
     email: {
         type: String,
-        required: true
+        required: true,
     },
     profilePicture: {
         type: String,
-        required: true
+        required: true,
     },
     role: {
         type: userRole,
-        default: userRole.Reader
+        default: userRole.Reader,
     },
     submissions: {
         type: Array<Schema.Types.ObjectId>,
-        ref: "Content"
-    }
+        ref: "Content",
+    },
 });
 
 export interface IUser {
-    _id?: string,
-    userID: number,
-    name: string,
-    email: string,
-    profilePicture: string, // probably a link to profile pic provided by Google
-    role?: userRole,
-    submissions: any[]
+    _id?: string;
+    userID: number;
+    name: string;
+    email: string;
+    profilePicture: string; // probably a link to profile pic provided by Google
+    role?: userRole;
+    submissions: any[];
 }
 
 export const User = model<IUser>("User", userSchema);
