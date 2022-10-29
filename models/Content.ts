833d80bd79ab3007ae5da38d926762b4b257bbc8
karankashyap04 <karankashyap@gmail.com>
2022-10-29 15:47:52 -0400

    Add new line at the end of the file

--- models/Content.ts
+++ models/Content.ts
@@ -1,67 +1,67 @@
-import { model, Schema } from 'mongoose';
+import { model, Schema } from "mongoose";
 
 enum contentType {
-    Text = 0,
-    Image = 1,
-    Video = 2
-};
+  Text = 0,
+  Image = 1,
+  Video = 2,
+}
 
 enum contentColumn {
-    Left = 0, 
-    Middle = 1,
-    Right = 2
-};
+  Left = 0,
+  Middle = 1,
+  Right = 2,
+}
 
 const contentSchema = new Schema({
-    userID: {
-        type: Number,
-        required: true
-    },
-    contentID: {
-        type: Number,
-        required: true
-    },
-    title: {
-        type: String,
-        required: true
-    },
-    author: {
-        type: Schema.Types.ObjectId,
-        ref: "User"
-    },
-    nodeType: {
-        type: contentType,
-        required: true
-    },
-    content: {
-        type: String,
-        required: true
-    },
-    column: {
-        type: contentColumn,
-        required: true
-    },
-    dateCreated: {
-        type: Date,
-        default: Date.now
-    },
-    lastUpdated: {
-        type: Date,
-        default: Date.now
-    }
+  userID: {
+    type: Number,
+    required: true,
+  },
+  contentID: {
+    type: Number,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  nodeType: {
+    type: contentType,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  column: {
+    type: contentColumn,
+    required: true,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
+  lastUpdated: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 export interface IContent {
-    _id?: string,
-    userID: number,
-    contentID: number,
-    title: string,
-    author: any,
-    nodeType: contentType,
-    content: string,
-    column: contentColumn,
-    dateCreated: Date,
-    lastUpdated: Date
+  _id?: string;
+  userID: number;
+  contentID: number;
+  title: string;
+  author: any;
+  nodeType: contentType;
+  content: string;
+  column: contentColumn;
+  dateCreated: Date;
+  lastUpdated: Date;
 }
 
 export const Content = model<IContent>("Content", contentSchema);
