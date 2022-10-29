2b3ee3b5dd95637718b4a5887f0604749b5a8cdf
karankashyap04 <karankashyap@gmail.com>
2022-10-29 16:42:36 -0400

    Remove redundant type checks in login.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,30 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import { useSession, signIn, signOut } from 'next-auth/react';
-
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Login() {
-    const {data: session} = useSession();
+    const { data: session } = useSession();
 
     if (session) {
         // already logged in
-        if (session.user === undefined) {
-            console.log("This should never happen!")
-            return <div>An unexpected error occurred while logging you in!</div>
-        } else {
-            return (
-                <div>
-                    <h2>Welcome, {session.user.name}</h2>
-                    <button onClick={() => signOut()}>Sign Out</button>
-                </div>
-            )
-        }
-    } else {
         return (
             <div>
-                <h2>You are not signed in!</h2>
-                <button onClick={() => signIn()}>Sign In</button>
+                <h2>Welcome, {session.user!.name}</h2>
+                <button onClick={() => signOut()}>Sign Out</button>
             </div>
-        )
+        );
     }
+    return (
+        <div>
+            <h2>You are not signed in!</h2>
+            <button onClick={() => signIn()}>Sign In</button>
+        </div>
+    );
 }
