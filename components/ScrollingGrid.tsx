743bc6865e5b44ec399634a7ff2a950d0ac8e474
jameshu111 <51237606+jameshu111@users.noreply.github.com>
2022-11-20 22:35:24 -0500

    scrolling grid basics

---
+++ components/ScrollingGrid.tsx
@@ -0,0 +1,166 @@
+import React, { useEffect, useRef, useState } from "react";
+
+import styles from "../styles/ScrollingGrid.module.css";
+
+const cellHeight = 16; //em
+const cellMargin = 1; //em
+
+// returns the breakpoint from a given screen width
+const getBreakpoint = (size: number): string => {
+    if (size >= 0 && size < 576) {
+        return "xs";
+    } else if (size >= 576 && size < 768) {
+        return "sm";
+    } else if (size >= 768 && size < 992) {
+        return "md";
+    } else if (size >= 992 && size < 1200) {
+        return "xl";
+    } else {
+        return "xxl";
+    }
+};
+
+// figures out how many columns we should have based on breakpoints
+const getNumColumns = (size: number): number => {
+    let breakpoint: string = getBreakpoint(size);
+    if (breakpoint === "xs") {
+        return 1;
+    } else if (breakpoint === "sm" || breakpoint === "md") {
+        return 2;
+    } else {
+        return 3;
+    }
+};
+
+interface ScrollingGridProps {
+    data: string[];
+    width?: string;
+    height?: string;
+}
+
+function ScrollingGrid(props: ScrollingGridProps) {
+    const [columns, setColumns] = useState<string[][]>([[]]);
+    const [scrollDelta, setScrollDelta] = useState(0);
+
+    const containerDivRef = useRef<HTMLDivElement>(null);
+
+    const getMaxColumnLength = (): number => {
+        let max = columns[0].length;
+        for (let i = 0; i < columns.length; i++) {
+            max = Math.max(max, columns[i].length);
+        }
+        return max;
+    };
+
+    const divideData = (width: number) => {
+        let cols: number = getNumColumns(width);
+        let dividedData: string[][] = [];
+
+        for (let i = 0; i < cols; i++) {
+            dividedData[i] = [];
+        }
+
+        //evenly distribute the items to each of the new columns
+        for (let i = 0; i < props.data.length; i++) {
+            dividedData[i % cols].push(props.data[i]);
+        }
+
+        setColumns(dividedData);
+    };
+
+    const updateDimensions = () => {
+        const width = window.innerWidth;
+        divideData(width);
+    };
+
+    const handleScroll = (e: React.UIEvent) => {
+        // console.log(containerDivRef.current!.scrollTop);
+        setScrollDelta(containerDivRef.current!.scrollTop);
+    };
+
+    useEffect(() => {
+        divideData(window.innerWidth);
+        window.addEventListener("resize", updateDimensions);
+        return () => window.removeEventListener("resize", updateDimensions);
+    }, []);
+
+    return (
+        <div
+            className={styles.container}
+            style={{
+                width: `${props.width}`,
+                height: `${props.height}`,
+                margin: "1em auto",
+            }}
+            ref={containerDivRef}
+            onScroll={(e) => handleScroll(e)}
+        >
+            {columns &&
+                columns.map((array: string[], index: number) => (
+                    <Column
+                        index={index}
+                        scrollDelta={scrollDelta}
+                        height={`${
+                            getMaxColumnLength() * (cellHeight + cellMargin)
+                        }em`}
+                        gridHeight={props.height}
+                    >
+                        {array.map((entry: string, index: number) => (
+                            <div
+                                className={styles.item}
+                                key={index}
+                                style={{ height: `${cellHeight}em` }}
+                            >
+                                <img src={entry} className={styles.cellImage} />
+                            </div>
+                        ))}
+                    </Column>
+                ))}
+        </div>
+    );
+}
+
+interface ColumnProps {
+    index: number;
+    scrollDelta: number;
+    children: React.ReactNode;
+    height?: string;
+    gridHeight?: string;
+}
+function Column(props: ColumnProps) {
+    if (props.index % 2 == 0) {
+        return (
+            <div
+                key={props.index}
+                className={styles.column}
+                style={{ height: props.height }}
+            >
+                {props.children}
+            </div>
+        );
+    } else {
+        let initialOffset: string = `calc(-1 * (${props.height} - ${props.gridHeight}))`;
+        console.log(initialOffset);
+        return (
+            <div
+                key={props.index}
+                className={styles.columnReverse}
+                style={{
+                    height: props.height,
+                }}
+            >
+                <div
+                    className={styles.columnReverseContainer}
+                    style={{
+                        marginTop: initialOffset,
+                        transform: `translateY(calc(2*${props.scrollDelta}px))`,
+                    }}
+                >
+                    {props.children}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ScrollingGrid;
