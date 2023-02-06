e7c44ccd541c794c0bd718b32750069331e579cb
Andrew Li <Andrewli2024@gmail.com>
2023-02-05 23:25:18 -0500

    Update about page to see if CI/CD works

--- pages/about.tsx
+++ pages/about.tsx
@@ -9,6 +9,7 @@ const About: NextPage = () => {
             <h1>This is Carte Blanche</h1>
             <h1>Our Mission</h1>
             <p>Lorem epsum..</p>
+            <h1>Meet the Team!</h1>
         </>
     );
 };
