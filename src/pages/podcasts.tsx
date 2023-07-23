585a1a22e26143e040d8ad565999de08ee10f57e
Andrew Li <Andrewli2024@gmail.com>
2023-07-23 15:42:58 -0400

    Create podcasts page

---
+++ src/pages/podcasts.tsx
@@ -0,0 +1,11 @@
+import TopNav from "@CarteBlanche/components/TopNav";
+import React from "react";
+
+export default function podcasts() {
+  return (
+    <>
+      <TopNav />
+      <div>podcasts</div>;
+    </>
+  );
+}
